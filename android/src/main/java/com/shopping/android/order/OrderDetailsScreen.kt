b5ef889557d32b1dfc4fac15db93dc014862b517
Ali Albaali <ali@albaali.com>
2020-12-23 20:28:38 +0300

    Add Android order components

---
+++ android/src/main/java/com/shopping/android/order/OrderDetailsScreen.kt
@@ -0,0 +1,11 @@
+package com.shopping.android.order
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.shopping.common.domain.model.Order
+
+@Composable
+fun OrderDetailsScreen(navController: NavController) {
+
+
+}
---
+++ android/src/main/java/com/shopping/android/order/OrdersScreen.kt
@@ -0,0 +1,12 @@
+package com.shopping.android.order
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+
+@Composable
+fun OrdersScreen(navController: NavController) {
+
+
+
+}
